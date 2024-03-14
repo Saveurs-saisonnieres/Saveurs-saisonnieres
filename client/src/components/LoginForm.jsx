cf97c0dbe8eec2d5cf1ede92b0abb7937adb297d
LouisKzy <louiskrejezy@gmail.com>
2024-03-14 16:12:22 +0100

    switch from bootstrap to M ui

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,18 @@
+
+// import de logique pour les composants
 import Cookies from "js-cookie";
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import { LoginFetch } from '../services/authService';
 import { useDispatch, useSelector } from "react-redux";
 import { loginSuccess } from "../features/authSlice";
 
-
+// composent M Ui
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 export default function LoginFrom() {
   const [formData, setFormData] = useState({
     email: '',
@@ -41,23 +47,31 @@ export default function LoginFrom() {
     }
   };
 
-  
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange} autoComplete='email' />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} autoComplete='current-password' />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Box>
+        <Box>
+          <Typography variant="h3" gutterBottom>
+            Connexion
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <TextField id="outlined-basic" label="email" value={formData.email} variant="outlined" onChange={handleChange} name="email" />
+              <TextField id="outlined-basic" label="password" value={formData.password} variant="outlined" onChange={handleChange} name="password" />
+              <Button variant="contained"type="submit">Login</Button>
+              <Box>
+                <Box>
+                  <Link href="/register">Not registered yet ?</Link>
+                </Box>
+                <Box>
+                  <Link href="/password/reset">Forgot password ?</Link>
+                </Box>
+              </Box>
+            </FormControl>
+          </form>
+        </Box>
+      </Box>
+    </>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,22 @@
+import Grid from '@mui/material/Grid';
 import LoginForm from '../components/LoginForm';
-function Login() {
 
+export default function LoginPage() {
   return (
-    <>
-      <h1>Login</h1>
-      <LoginForm />
-      <a href="/register">Not registered yet ?</a>
-      {" | "}
-      <a href="/password/reset">Forgot password ?</a>
-    </>
+    <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+      {/* Colonne de gauche (vide dans cet exemple) */}
+      <Grid item xs={6}>
+        {/* Contenu de la colonne de gauche */}
+      </Grid>
+
+      {/* Colonne de droite (contenant le formulaire de connexion) */}
+      <Grid item xs={6}>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <LoginForm />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
-
-export default Login;
