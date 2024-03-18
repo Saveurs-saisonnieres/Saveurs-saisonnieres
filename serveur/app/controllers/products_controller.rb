53aa1ee47a28e835aad3d28ad6fe96d1f2ef7325
LouisKzy <louiskrejezy@gmail.com>
2024-03-18 18:05:33 +0100

    product and cart protect by auth

--- serveur/app/controllers/products_controller.rb
+++ serveur/app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_admin, only: [:create, :update, :destroy]
+  before_action :set_product, only: [:show, :update, :destroy]
 
   # GET /products
   def index
@@ -34,9 +36,15 @@ class ProductsController < ApplicationController
   end
 
   # DELETE /products/1
+# DELETE /products/1
   def destroy
-    @product.destroy!
+    if @product.destroy
+      head :no_content
+    else
+      render json: { error: 'Unable to destroy product' }, status: :unprocessable_entity
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -47,5 +55,11 @@ class ProductsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def product_params
       params.require(:product).permit(:name, :img, :price, :description, :origin, :variety, :categorie, :quantity)
+    end
+
+    def authorize_admin
+      unless current_user & current_user.admin?
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
 end
