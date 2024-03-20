bf6ca16b7dc163914d24ac9e7e782e13835835af
Louis Krejezy <146070864+LouisKzy@users.noreply.github.com>
2024-03-20 16:25:44 +0100

    Update products_controller.rb

--- serveur/app/controllers/products_controller.rb
+++ serveur/app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show]
-  # before_action :authorize_admin, only: [:create, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_admin, only: [:create, :update, :destroy]
   before_action :set_product, only: [:show, :update, :destroy]
 
   # GET /products
