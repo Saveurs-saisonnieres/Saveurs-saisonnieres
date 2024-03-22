f138c01a6871dcc6db106a25ef7f8b4e41d7f0f1
LouisKzy <louiskrejezy@gmail.com>
2024-03-22 17:49:17 +0100

    index produit front

---
+++ client/src/components/ShowProduct.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { AddProductToCart } from "../services/cartServices";
+import { useParams } from "react-router-dom";
+
+const ShowProducts = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/products/${id}`
+        );
+        setProduct(response.data);
+        console.log("Product:", response.data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+
+    fetchProduct();
+  }, [id]); 
+
+  return (
+    <div>
+      <h2>Product Details</h2>
+      {product && (
+        <div>
+          <h3>{product.name}</h3>
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+          <p>Origin: {product.origin}</p>
+          <p>Variety: {product.variety}</p>
+          {product.img_url && (
+            <img src={"http://localhost:3000/" + product.img_url} alt="Product" />
+          )}
+          <button onClick={() => AddProductToCart(product.id)}>Add to cart</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowProducts;
