8fca6e4da43f65c97d8ea6bbf50e7ba3edc186dd
100PBaguuette <magnat.gregory@gmail.com>
2024-03-22 15:01:47 +0100

    Commentaire Mailjet

--- serveur/app/mailers/user_mailer.rb
+++ serveur/app/mailers/user_mailer.rb
@@ -8,22 +8,22 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez Saveurs Saisonnières !') 
   end
 
-  def ???_confirmation_email(user, ????)
-    @user = user
-
-    @??? = ???
-
-    mail(to: @user.email, subject: 'Confirmation : Votre commande chez Saveurs Saisonières est bien prise en compte.')
-  end
-
-  def user_admin_notification(user_admin_email, cart???)
-    @user_admin_email = user_admin_email
-    @user_admin = User.find_by(email: @user_admin_email)
-
-    @cart??? = cart???
-
-    mail(to: @user_admin_email, subject: 'Notification : Une nouvelle commande est en cours !')
-  end
+# def ???_confirmation_email(user, ????)
+#   # @user = user
+# 
+#   # @??? = ???
+# 
+#   # mail(to: @user.email, subject: 'Confirmation : Votre commande chez Saveurs Saisonières est bien prise en compte.')
+# end
+# 
+# def user_admin_notification(user_admin_email, cart???)
+#   # @user_admin_email = user_admin_email
+#   # @user_admin = User.find_by(email: @user_admin_email)
+# 
+#   # @cart??? = cart???
+# 
+#   # mail(to: @user_admin_email, subject: 'Notification : Une nouvelle commande est en cours !')
+# end
 
   def reset_password_instructions(user)
     @user = user
