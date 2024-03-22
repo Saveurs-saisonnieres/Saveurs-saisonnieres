2319921edbe7cc553dc671021e15355208593818
LouisKzy <louiskrejezy@gmail.com>
2024-03-22 17:48:13 +0100

    index produit mui

--- serveur/app/controllers/users/registrations_controller.rb
+++ serveur/app/controllers/users/registrations_controller.rb
@@ -34,4 +34,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+  end
 end
--- serveur/app/models/user.rb
+++ serveur/app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable, :jwt_authenticatable,
         jwt_revocation_strategy: JwtDenylist
   validates :email, presence: true
+
   has_one :cart
+
 end
