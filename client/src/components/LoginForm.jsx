7a15b7fb3ff38c9c137a2358d9b0fe8a202c8fcf
LouisKzy <louiskrejezy@gmail.com>
2024-03-29 14:13:32 +0100

    fix issue in login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,23 +1,13 @@
-// import * as React from 'react';
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Cookies from "js-cookie";
 import { useRef } from "react";
-import { LoginFetch } from "../services/authService";
 import { useDispatch } from "react-redux";
+import { LoginFetch } from "../services/authService";
 import { login } from "../features/authSlice";
-
+import Cookies from "js-cookie";
 import Logo from "../assets/images/LogoLog.svg";
 import Fonregister from "../assets/images/Fonregister.jpg";
+import { Grid, Paper, Box, Typography, TextField, FormControlLabel, Checkbox, Button, Link } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function LoginForm() {
   const emailRef = useRef("");
@@ -32,12 +22,20 @@ export default function LoginForm() {
     try {
       const { data, headers } = await LoginFetch(email, password);
       const user = data.user;
-      Cookies.set("useradmin", user.admin);
       const token = headers.authorization;
+
       Cookies.set("token", token);
       dispatch(login({ token: token, isAdmin: user.admin }));
-      console.log("Successfully logged in : ", data.message);
-      window.location.href = "/products";
+
+      if (user.admin) {
+        Cookies.set("useradmin", user.admin);
+        console.log("Successfully logged in as admin: ", data.message);
+        window.location.href = "/admin/products";
+      } else {
+        dispatch(login({ token: token, isAdmin: false }));
+        console.log("Successfully logged in as non-admin: ", data.message);
+        window.location.href = "/products";
+      }
     } catch (error) {
       console.error("Failed to login:", error.message);
     }
